00cde22d738cb2384b5e4bf73c0f1da1c9cdfe0f
VanYoshiko <khorihenry@hotmail.com>
2026-01-17 17:20:31 +0000

    removed unwanted files

--- bin/cheatsheet.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-Download to a specified file
-wget https://tldp.org/LDP/abs/html/sample-bashrc.html -P /mnt/Downloads/Docs/html
-Download from youtube:
-yt-dlp -o "%(playlist)s/%(title)s.%(ext)s '<YT-URL>'"
--- notes_tmp.md
+++
@@ -1,4 +0,0 @@
-
-[Explain Shell.com](https://explainshell.com/)
-[VimHelpDocs](https://vimhelp.org/)
-[Commandlinefu.com](https://www.commandlinefu.com/commands/browse)
