98a509ceeaea3458002e7ac7dab03a68f2b9c550
VanYoshiko <khorihenry@hotmail.com>
2026-02-10 10:48:45 +0000

    Merged develop brach to master; committing changes of bash_... and sym...sh

--- symlink_ish.sh
+++ symlink_ish.sh
@@ -9,18 +9,18 @@ asci_colors="$home_dir/.local/xdg_state_home/terminal_graphics/.asci_colors"
 source $asci_colors
 
 symlink_ishfiles() {
-	ln -s "$home_dir/.bash_profile" "$HOME" && echo "[INFO] Symlinked .profile"
-	ln -s "$home_dir/.ssh" "$HOME" && echo "[INFO] Symlinked .ssh"
+	ln -sf "$home_dir/.bash_profile" "$HOME" && echo "[INFO] Symlinked .profile"
+	ln -sf "$home_dir/.ssh" "$HOME" && echo "[INFO] Symlinked .ssh"
 }
 
 symlink_aShell() {
 	local logger="$home_dir/.local/xdg_config_home/logger_a-Shell"
 	local log_file="$home_dir/.local/xdg_state_home/logs/a-Shell_logs/a-Shell_logs"
 	. "$logger" "Symlink manager" "INFO"
-	ln -s "$home_dir/.profile" "$HOME/Documents" && echo "[INFO] Symlinked .profile"
-	ln -s "$home_dir/.ssh" "$HOME/Documents" && echo "[INFO] Symlinked .ssh"
-	ln -s "$home_dir/.vim" "$HOME/Documents" && echo "[INFO] Symlinked .vim"
-	ln -s "$home_dir/.vimrc" "$HOME/Documents" && echo "[INFO] Symlinked .vimrc"
+	ln -sf "$home_dir/.profile" "$HOME/Documents" && echo "[INFO] Symlinked .profile"
+	ln -sf "$home_dir/.ssh" "$HOME/Documents" && echo "[INFO] Symlinked .ssh"
+	ln -sf "$home_dir/.vim" "$HOME/Documents" && echo "[INFO] Symlinked .vim"
+	ln -sf "$home_dir/.vimrc" "$HOME/Documents" && echo "[INFO] Symlinked .vimrc"
 }
 
 # printf "%s\t\t%s\n" "🟢🟡🔴" "SYMLINK DOTFILES"
